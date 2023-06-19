846a7c28169ba64fcbb1d0c7da8940c29e9581de
isaiasramosdeoliveira <isaias.ramos.olive@gmail.com>
2023-06-19 13:53:47 -0300

    dsd

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -38,7 +38,7 @@ const Header = () => {
               Partidos
             </Nav.Link>
             <Nav.Link style={linkStyled} href="/proposicoes">
-              Proposicoes
+              Proposicões
             </Nav.Link>
             <Nav.Link style={linkStyled} href="/votacoes">
               Votações
